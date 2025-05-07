385764edaeec88f45ce2e121dab8e2ae5833ff14
sarvamangala-c <sarvamangala123456789@gmail.com>
2025-05-07 15:28:29 +0530

    save

---
+++ Basicoperations/Basicoperations.java
@@ -0,0 +1,13 @@
+//Basicoperations.java
+public class Basicoperations{
+    public static void main(String[] args){
+        int a =15;
+        int b=5;
+        int sum =a+b;
+        int diff=a-b;
+        int prod=a*b;
+        System.out.println("Addation: "+sum);
+        System.out.println("substrsction: "+diff);
+        System.out.println("Multiplication: "+prod);
+    }
+}
